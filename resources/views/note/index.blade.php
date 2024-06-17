c5bdd6419db5200f2122d9f85ddeeb02b49e3fcb
Nawshin Nasir <nawshin22nasir@gmail.com>
2024-06-17 19:08:38 +0600

    initial layout

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <h1>Index</h1>
-</div>
+<x-layout>
+    <div class="note-container">
+        <a href="#" class="new-notr-btn">
+            New Note
+        </a>
+        <div class="notes">
+            <div class="note">
+                <div class="note-body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aliquid aperiam animi fugiat perferendis ea fugit officiis, quas aspernatur tempore? Suscipit, earum ipsam! Sit quam minus assumenda, dignissimos dolores aliquid.
+                </div>
+                <div class="note-buttons">
+                    <a href="#" class="note-view-button">View</a>
+                    <a href="#" class="note-edit-button">Edit</a>
+                    <button class="note-delete-button">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
